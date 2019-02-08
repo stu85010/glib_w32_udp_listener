90da5ecc554b50c89664d91b1ac74c7bc4359de6
Michael Tang <michaelok.ja@gmail.com>
2019-02-08 15:35:41 +0800

    WIP: extract g_io_channel_win32_make_pollfd method from GLib giowin32.c(2.52.3) without G_IO_WIN32_FILE_DESC and G_IO_WIN32_CONSOLE support

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 
-
+#define _WINSOCKAPI_
 #include <windows.h>
 
 #include <string>
@@ -34,6 +34,152 @@ static gboolean udp_received(GSocket *sock, GIOCondition condition, gpointer dat
 	return TRUE;
 }
 
+#pragma region FIXED_WIN32_POLL
+
+#include <WinSock2.h>
+#if 1
+typedef struct _GIOWin32Channel GIOWin32Channel;
+typedef struct _GIOWin32Watch GIOWin32Watch;
+
+#define BUFFER_SIZE 4096
+
+typedef enum {
+	G_IO_WIN32_WINDOWS_MESSAGES,	/* Windows messages */
+
+	G_IO_WIN32_FILE_DESC,		/* Unix-like file descriptors from
+								* _open() or _pipe(), except for
+								* console IO. Separate thread to read
+								* or write.
+								*/
+
+	G_IO_WIN32_CONSOLE,		/* Console IO (usually stdin, stdout, stderr) */
+
+	G_IO_WIN32_SOCKET		/* Sockets. No separate thread. */
+} GIOWin32ChannelType;
+
+struct _GIOWin32Channel {
+	GIOChannel channel;
+	gint fd;			/* Either a Unix-like file handle as provided
+						* by the Microsoft C runtime, or a SOCKET
+						* as provided by WinSock.
+						*/
+	GIOWin32ChannelType type;
+
+	gboolean debug;
+
+	/* Field used by G_IO_WIN32_WINDOWS_MESSAGES channels */
+	HWND hwnd;			/* Handle of window, or NULL */
+
+						/* Fields used by G_IO_WIN32_FILE_DESC channels. */
+	CRITICAL_SECTION mutex;
+
+	int direction;		/* 0 means we read from it,
+						* 1 means we write to it.
+						*/
+
+	gboolean running;		/* Is reader or writer thread
+							* running. FALSE if EOF has been
+							* reached by the reader thread.
+							*/
+
+	gboolean needs_close;		/* If the channel has been closed while
+								* the reader thread was still running.
+								*/
+
+	guint thread_id;		/* If non-NULL the channel has or has
+							* had a reader or writer thread.
+							*/
+	HANDLE data_avail_event;
+
+	gushort revents;
+
+	/* Data is kept in a circular buffer. To be able to distinguish between
+	* empty and full buffers, we cannot fill it completely, but have to
+	* leave a one character gap.
+	*
+	* Data available is between indexes rdp and wrp-1 (modulo BUFFER_SIZE).
+	*
+	* Empty:    wrp == rdp
+	* Full:     (wrp + 1) % BUFFER_SIZE == rdp
+	* Partial:  otherwise
+	*/
+	guchar *buffer;		/* (Circular) buffer */
+	gint wrp, rdp;		/* Buffer indices for writing and reading */
+	HANDLE space_avail_event;
+
+	/* Fields used by G_IO_WIN32_SOCKET channels */
+	int event_mask;
+	int last_events;
+	HANDLE event;
+	gboolean write_would_have_blocked;
+	gboolean ever_writable;
+};
+
+struct _GIOWin32Watch {
+	GSource       source;
+	GPollFD       pollfd;
+	GIOChannel   *channel;
+	GIOCondition  condition;
+};
+
+void fixed_g_io_channel_win32_make_pollfd(GIOChannel   *channel,
+	GIOCondition  condition,
+	GPollFD      *fd)
+{
+	GIOWin32Channel *win32_channel = (GIOWin32Channel *)channel;
+
+	switch (win32_channel->type)
+	{
+	//case G_IO_WIN32_FILE_DESC:
+	//	if (win32_channel->data_avail_event == NULL)
+	//		create_events(win32_channel);
+
+	//	fd->fd = (gintptr)win32_channel->data_avail_event;
+
+	//	if (win32_channel->thread_id == 0)
+	//	{
+	//		/* Is it meaningful for a file descriptor to be polled for
+	//		* both IN and OUT? For what kind of file descriptor would
+	//		* that be? Doesn't seem to make sense, in practise the file
+	//		* descriptors handled here are always read or write ends of
+	//		* pipes surely, and thus unidirectional.
+	//		*/
+	//		if (condition & G_IO_IN)
+	//			create_thread(win32_channel, condition, read_thread);
+	//		else if (condition & G_IO_OUT)
+	//			create_thread(win32_channel, condition, write_thread);
+	//	}
+	//	break;
+
+	//case G_IO_WIN32_CONSOLE:
+	//	fd->fd = _get_osfhandle(win32_channel->fd);
+	//	break;
+
+	case G_IO_WIN32_SOCKET:
+		fd->fd = (gintptr)WSACreateEvent();
+		break;
+
+	case G_IO_WIN32_WINDOWS_MESSAGES:
+		fd->fd = G_WIN32_MSG_HANDLE;
+		break;
+
+	default:
+		g_assert_not_reached();
+		g_abort();
+	}
+
+	fd->events = condition;
+}
+#else
+void fixed_g_io_channel_win32_make_pollfd(GIOChannel   *channel,
+	GIOCondition  condition,
+	GPollFD      *fd)
+{
+}
+#endif
+
+#pragma endregion
+
 void socket_hook_pollfd(GSocket *socket)
 {
 	gint fd = g_socket_get_fd(socket);
@@ -41,7 +187,8 @@ void socket_hook_pollfd(GSocket *socket)
 
 #ifdef _MSC_VER
 	GIOChannel *channel = g_io_channel_win32_new_socket(fd);
-	g_io_channel_win32_make_pollfd(channel, G_IO_IN, poll_fds);
+	//g_io_channel_win32_make_pollfd(channel, G_IO_IN, poll_fds);
+	fixed_g_io_channel_win32_make_pollfd(channel, G_IO_IN, poll_fds);
 #else
 	GIOChannel *channel = g_io_channel_unix_new(fd);
 
